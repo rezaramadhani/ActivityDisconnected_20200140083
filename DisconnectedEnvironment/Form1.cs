c2d29071a6a62ba429a9e469f1e752048ab2c798
rezaramadhani <rezarmd17@gmail.com>
2022-06-11 19:19:42 +0700

    Add project files.

---
+++ DisconnectedEnvironment/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisconnectedEnvironment
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt;
+        DataRow dr;
+        string Code;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'hRDataSet.empdetails' table. You can move, or remove it, as needed.
+            this.empdetailsTableAdapter.Fill(this.hRDataSet.empdetails);
+           
+            // TODO: This line of code loads data into the 'hRDataSet.empdetails' table. You can move, or remove it, as needed.
+            //This line of code loads data into the hRDataSet.Empdetails table. this would appear in form 1 load event
+            txtCode.Enabled = false;
+            txtName.Enabled = false;
+            txtAddress.Enabled = false;
+            txtState.Enabled = false;
+            txtCountry.Enabled = false;
+            cbDesignation.Enabled = false;
+            cbDepartment.Enabled = false;
+            cbDesignation.Items.Add("MANAGER");
+            cbDesignation.Items.Add("AUTHOR");
+            cbDesignation.Items.Add("DESIGNER");
+            cbDepartment.Items.Add("MARKETING");
+            cbDepartment.Items.Add("FINANCE");
+            cbDepartment.Items.Add("IDD");
+            cmdSave.Enabled = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdSave_Click(object sender, EventArgs e)
+        {
+            
+
+            dr = dt.NewRow();
+            dr[0] = txtCode.Text;
+            dr[1] = txtName.Text;
+            dr[2] = txtAddress.Text;
+            dr[3] = txtState.Text;
+            dr[4] = txtCountry.Text;
+            dr[5] = cbDesignation.SelectedItem;
+            dr[6] = cbDepartment.SelectedItem;
+            dt.Rows.Add(dr);
+            txtCode.Text = System.Convert.ToString(dr[0]);
+            txtCode.Enabled = false;
+            txtName.Enabled = false;
+            txtAddress.Enabled = false;
+            txtState.Enabled = false;
+            txtCountry.Enabled = false;
+            cbDesignation.Enabled = false;
+            cbDepartment.Enabled = false;
+            cmdAdd.Enabled = true;
+            cmdSave.Enabled = false;
+        }
+
+        private void cmdDelete_Click(object sender, EventArgs e)
+        {
+            string code;
+            code = txtCode.Text;
+            dr = hRDataSet.Tables["empedetails"].Rows.Find(code);
+            dr.Delete();
+            empdetailsTableAdapter.Update(hRDataSet);
+        }
+    }
+}
